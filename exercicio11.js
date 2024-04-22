fc699ec130480d4c50eb3461bb3a19166bff6112
RogerCorb <rogeriocgprest@gmail.com>
2024-04-22 19:01:14 -0300

    retirado bloco ifelse por ternário

--- exercicio11.js
+++ exercicio11.js
@@ -7,14 +7,12 @@ while (i === 0) {
     const numeroInteiro = parseInt(prompt('Digite um numero inteiro.: '));
 
     if (numeroInteiro > 0 && numeroInteiro!==null && !isNaN(numeroInteiro)) {
-        if (numeroInteiro%2 === 0 ) {
-            tipoNumero='par';       
-        } else {
-            tipoNumero='impar';        
-        }
+
+        tipoNumero = numeroInteiro%2===0 ? 'par' : 'impar' ;      
         console.log(`O numero ${numeroInteiro} é ${tipoNumero} .`);        
     } else {
         i = 1;
         console.log('Saindo do programa ...');
-    }        
+    }
+    console.log('Digite QQ tecla para sair, para continuar digite um proximo número');        
 }
