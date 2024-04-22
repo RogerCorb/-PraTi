29692b828aac4caa9435a22dcc34850f917df506
RogerCorb <rogeriocgprest@gmail.com>
2024-04-22 18:36:36 -0300

    refatorando e adicionando ; na linha 23

--- exercicio09.js
+++ exercicio09.js
@@ -12,7 +12,6 @@ matriz[6] = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, "Centro-Oeste"];
 matriz[7] = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, "Nordeste"];
 
 const codigoProduto = parseInt(prompt('Digite o codigo de Origem.: '));
-let index;
 let regiaoEncontrada = '';
 
 if (codigoProduto > 20 && codigoProduto < 25 || codigoProduto > 50) {
@@ -21,7 +20,7 @@ if (codigoProduto > 20 && codigoProduto < 25 || codigoProduto > 50) {
 } else {
 
     for (const vetorIterator of matriz) {       
-        index = vetorIterator.find(element=>element===codigoProduto) 
+        const index = vetorIterator.find(element=>element===codigoProduto); 
         if (index) {           
             regiaoEncontrada = vetorIterator[vetorIterator.length-1];
             break;
